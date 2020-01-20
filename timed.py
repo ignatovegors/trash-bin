1a73fb89c51b7cb3b27c7953c33574e5240d3fc8
ignatovegors <60047566+ignatovegors@users.noreply.github.com>
2020-01-20 15:22:26 +0300

    Create timed.py

---
+++ timed.py
@@ -0,0 +1,11 @@
+import time
+
+
+def timed(f, *args, n_iter=100):
+    acc = float("inf")
+    for i in range(n_iter):
+        t0 = time.perf_counter()
+        f(*args)
+        t1 = time.perf_counter()
+        acc = min(acc, t1 - t0)
+    return acc
